74de290c7921a2aece32f51d799d83ada4a50226
MartinStankov <martin.stankov13@gmail.com>
2025-08-01 15:14:58 +0300

    Added another test file from Fedora

---
+++ 02.Fundamentals/12.AssociativeArrays/src/anotherTestFile.java
@@ -0,0 +1,8 @@
+public class anotherTestFile {
+    public static void main(String[] args) {
+        String testMessage = "TestMessage from Fedora";
+        for (int i = 0; i < 100; i++) {
+            System.out.println(testMessage + " " + i);
+        }
+    }
+}
